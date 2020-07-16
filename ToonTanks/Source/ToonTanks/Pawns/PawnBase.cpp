5917d484ea5a82943474c93a58c2fc0ca27f6349
mastermarchewa3454/Battle_Tank_Game <mastermarchewa3454@users.noreply.github.com>
2020-07-16 19:10:07 +0200

    Set up blueprints

--- ToonTanks/Source/ToonTanks/Pawns/PawnBase.cpp
+++ ToonTanks/Source/ToonTanks/Pawns/PawnBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PawnBase.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 APawnBase::APawnBase()
@@ -9,6 +10,17 @@ APawnBase::APawnBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capture Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(RootComponent);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Project Spawn Point"));
+	ProjectSpawnPoint->SetupAttachment(TurretMesh);
 }
 
 // Called when the game starts or when spawned
